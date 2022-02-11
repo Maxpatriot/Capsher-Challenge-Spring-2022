fc4b91b47d18afd31f44128578db13bb750536e1
Maxpatriot <americana2013@gmail.com>
2022-02-11 04:13:14 -0600

    Created new flutter project

---
+++ capsher_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: const Text('Welcome to the app'),
+        ),
+        body: Center(
+          child: Container(
+            child: const Text('Hi there!'),
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            color: Colors.red,
+            width: 300,
+          ),
+        ),
+      ),
+    );
+  }
+}
