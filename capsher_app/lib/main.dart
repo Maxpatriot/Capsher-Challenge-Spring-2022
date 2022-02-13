f82f84cc38d327d7ab4b4a7663d36e874f27a578
WaaizK <waaizkhann@gmail.com>
2022-02-12 21:38:34 -0600

    changed blue to red

--- capsher_app/lib/main.dart
+++ capsher_app/lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +15,19 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.orange,
           title: const Text('Welcome to the app'),
+          foregroundColor: Colors.red,
         ),
         body: Center(
           child: Container(
             child: const Text('Hi there!'),
             margin: const EdgeInsets.all(10),
             padding: const EdgeInsets.all(10),
-            color: Colors.blue,
+            color: Colors.red,
             width: 300,
           ),
         ),
+
+        backgroundColor: Colors.grey,
       ),
     );
   }
